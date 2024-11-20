4abe319c653b6521176c20d405e0735574b84625
Zoltan000 <zoltanh66@gmail.com>
2024-11-20 01:06:41 -0500

    read more javascript

--- src/components/contact_section/Contact.tsx
+++ src/components/contact_section/Contact.tsx
@@ -27,18 +27,12 @@ export default function Contact({ contact, size }: ContactProps) {
       link: contact.linkedin,
       link_alt: "LinkedIn",
     },
-    /*{
-      name: "twitter",
-      icon: "fab fa-x-twitter",
-      link: contact.twitter,
-      link_alt: "Twitter",
-    },
-    {
-      name: "rss",
-      icon: "fas fa-rss",
-      link: "/rss.xml",
-      link_alt: "RSS Feed",
-    },*/
+    // {
+    //   name: "rss",
+    //   icon: "fas fa-rss",
+    //   link: "/rss.xml",
+    //   link_alt: "RSS Feed",
+    // },
   ];
 
   return (
